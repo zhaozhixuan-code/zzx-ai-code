82e8483113e169283d2cec899c84b1d6af938a7e
laplands <928215814@qq.com>
2025-11-19 18:56:18 +0800

    初始化后端项目

---
+++ src/main/java/com/zzx/zzxaicode/ZzxAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.zzx.zzxaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzxAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzxAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzx/zzxaicode/ZzxAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzx.zzxaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzxAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

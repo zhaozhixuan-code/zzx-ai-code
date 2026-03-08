89b762f31a984d3cbaf9bbf91f249da867e80a39
laplands <928215814@qq.com>
2026-03-08 22:26:41 +0800

    AI 工作流（LangGraph4j 实现网站生成工作流）
    编写简单工作流图、有状态工作流图、模拟状态流转工作流图，渐进式开发
    开发图片收集工具和 AI 服务
    开发各个工作节点
    开发实际应用的代码生成工作流

---
+++ src/main/java/com/zzx/zzxaicode/langgraph4j/ai/ImageCollectionServiceFactory.java
@@ -0,0 +1,51 @@
+package com.zzx.zzxaicode.langgraph4j.ai;
+
+import com.zzx.zzxaicode.langgraph4j.tools.ImageSearchTool;
+import com.zzx.zzxaicode.langgraph4j.tools.LogoGeneratorTool;
+import com.zzx.zzxaicode.langgraph4j.tools.MermaidDiagramTool;
+import com.zzx.zzxaicode.langgraph4j.tools.UndrawIllustrationTool;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AI 服务工厂，注入指定的 chatModel 和各种图片收集工具
+ */
+@Slf4j
+@Configuration
+public class ImageCollectionServiceFactory {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private ImageSearchTool imageSearchTool;
+
+    @Resource
+    private UndrawIllustrationTool undrawIllustrationTool;
+
+    @Resource
+    private MermaidDiagramTool mermaidDiagramTool;
+
+    @Resource
+    private LogoGeneratorTool logoGeneratorTool;
+
+    /**
+     * 创建图片收集 AI 服务
+     */
+    @Bean
+    public ImageCollectionService createImageCollectionService() {
+        return AiServices.builder(ImageCollectionService.class)
+                .chatModel(chatModel)
+                .tools(
+                        imageSearchTool,
+                        undrawIllustrationTool,
+                        mermaidDiagramTool,
+                        logoGeneratorTool
+                )
+                .build();
+    }
+}

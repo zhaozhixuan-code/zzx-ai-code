0dc0cb2616bcd9ecc2c6041a1e79312ef768a1c1
zhaozhixuan <53525430x@gmail.com>
2026-03-09 17:47:31 +0800

    LangGraph4j 基于LangGraph4j parallel Branch 实现并发

--- src/main/java/com/zzx/zzxaicode/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/zzx/zzxaicode/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.zzx.zzxaicode.langgraph4j.state;
 
+import com.zzx.zzxaicode.langgraph4j.model.ImageCollectionPlan;
 import com.zzx.zzxaicode.langgraph4j.model.ImageResource;
 import com.zzx.zzxaicode.langgraph4j.model.QualityResult;
 import com.zzx.zzxaicode.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,21 @@ public class WorkflowContext implements Serializable {
      * 错误信息
      */
     private String errorMessage;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
+
 
     @Serial
     private static final long serialVersionUID = 1L;

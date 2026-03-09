d423e7da49f0bb58aa8616c9a263e404b0592a99
zhaozhixuan <53525430x@gmail.com>
2026-03-09 21:37:56 +0800

    使用redis缓存精选应用

--- src/main/java/com/zzx/zzxaicode/ZzxAiCodeApplication.java
+++ src/main/java/com/zzx/zzxaicode/ZzxAiCodeApplication.java
@@ -4,7 +4,9 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 // 排除用于 RAG Embedding 模块
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @MapperScan("com.zzx.zzxaicode.mapper")
--- src/main/java/com/zzx/zzxaicode/controller/AppController.java
+++ src/main/java/com/zzx/zzxaicode/controller/AppController.java
@@ -24,6 +24,7 @@ import com.zzx.zzxaicode.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
@@ -55,7 +56,6 @@ public class AppController {
 
     @Resource
     private ProjectDownloadService projectDownloadService;
-
 
 
     /**
@@ -273,6 +273,11 @@ public class AppController {
      * @return 精选应用列表
      */
     @PostMapping("/good/list/page/vo")
+    @Cacheable(
+            value = "good_app_page",
+            key = "T(com.zzx.zzxaicode.utils.CacheKeyUtils).generateKey(#appQueryRequest)",
+            condition = "#appQueryRequest.pageNum <= 10"
+    )
     public BaseResponse<Page<AppVO>> listGoodAppVOByPage(@RequestBody AppQueryRequest appQueryRequest) {
         ThrowUtils.throwIf(appQueryRequest == null, ErrorCode.PARAMS_ERROR);
         // 限制每页最多 20 个

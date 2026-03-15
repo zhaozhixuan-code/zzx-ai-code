9712554094bbb2b62fbb31b735489558fd5027d1
zhaozhixuan <53525430x@gmail.com>
2026-03-15 21:59:13 +0800

    微服务改造拆分 - 服务接口模块（Client）

---
+++ zzx-ai-code-microservice/zzx-ai-code-client/src/main/java/com/zzx/zzxaicode/innerservice/InnerScreenshotService.java
@@ -0,0 +1,10 @@
+package com.zzx.zzxaicode.innerservice;
+
+/**
+ * 内部服务：截图服务
+ */
+public interface InnerScreenshotService {
+
+    String generateAndUploadScreenshot(String webUrl);
+
+}
---
+++ zzx-ai-code-microservice/zzx-ai-code-client/src/main/java/com/zzx/zzxaicode/innerservice/InnerUserService.java
@@ -0,0 +1,36 @@
+package com.zzx.zzxaicode.innerservice;
+
+import com.zzx.zzxaicode.exception.BusinessException;
+import com.zzx.zzxaicode.exception.ErrorCode;
+import com.zzx.zzxaicode.model.po.User;
+import com.zzx.zzxaicode.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.zzx.zzxaicode.constants.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 内部使用的用户服务
+ */
+public interface InnerUserService {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
+
